ee49dadde727130e3be6e16094fc7c102da60b4b
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-06 19:07:42 -0300

    feat: req 7 e 8 - add methods existword e searchbyword

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,48 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_occurence_count_list = []
+
+    for index in range(len(instance)):
+        file_info = instance.search(index)
+
+        lines_location_list = []
+
+        for i, line_content in enumerate(file_info["linhas_do_arquivo"]):
+            if word.lower() in line_content.lower():
+                lines_location_list.append(dict(linha=i + 1))
+
+        if len(lines_location_list) > 0:
+            word_occurence_count_list.append(
+                dict(
+                    palavra=word,
+                    arquivo=file_info["nome_do_arquivo"],
+                    ocorrencias=lines_location_list,
+                )
+            )
+
+    return word_occurence_count_list
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_occurence_count_list = []
+
+    for index in range(len(instance)):
+        file_info = instance.search(index)
+
+        lines_location_list = []
+
+        for i, line_content in enumerate(file_info["linhas_do_arquivo"]):
+            if word.lower() in line_content.lower():
+                lines_location_list.append(
+                    dict(linha=i + 1, conteudo=line_content)
+                )
+
+        if len(lines_location_list) > 0:
+            word_occurence_count_list.append(
+                dict(
+                    palavra=word,
+                    arquivo=file_info["nome_do_arquivo"],
+                    ocorrencias=lines_location_list,
+                )
+            )
+
+    return word_occurence_count_list

426c9ae59bb5cd4e0a9a3b5213069d80c4e21588
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-06 15:55:11 -0300

    feat: req 2 - add txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file[-4:] == ".txt":
+        error_msg = "Formato inválido"
+        sys.stderr.write(error_msg)
+        return None
+
+    try:
+        with open(path_file, "r") as file:
+            news_imported_list = file.read().split("\n")
+
+            return news_imported_list
+
+    except FileNotFoundError:
+        error_msg = f"Arquivo {path_file} não encontrado\n"
+        sys.stderr.write(error_msg)

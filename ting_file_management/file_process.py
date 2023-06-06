5815b396616868740c4502940aa9f6e5d2e33cfc
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-06 16:32:57 -0300

    feat: req 3 - add func process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_name_list = [file["nome_do_arquivo"] for file in instance._data]
+
+    if path_file in file_name_list:
+        return print(f"Arquivo {path_file} já foi processado.")
+
+    news_imported_list = txt_importer(path_file)
+    news_imported_dict = dict(
+        nome_do_arquivo=path_file,
+        qtd_linhas=len(news_imported_list),
+        linhas_do_arquivo=news_imported_list,
+    )
+    instance.enqueue(news_imported_dict)
+
+    sys.stdout.write(str(news_imported_dict))
 
 
 def remove(instance):

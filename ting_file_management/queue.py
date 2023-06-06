13780847ed297c30fe47bbb0a792ba3176661a82
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-06 15:26:54 -0300

    feat: req 1 - add classe queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,25 @@
 from ting_file_management.abstract_queue import AbstractQueue
 
 
+# referencias do código:
+# https://app.betrybe.com/learn/course/ secao 5.5 - Pilhas e Filas - filas
+#
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqu"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            return None
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self._data):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._data[index]
